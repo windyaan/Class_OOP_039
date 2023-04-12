c04ec5452fa61d5bd9e24ac3808269ac954dbca4
windyaan <windyanurmaretha@gmail.com>
2023-04-12 15:22:10 +0700

    Add project files.

---
+++ oop_039/oop_039.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+class Mahasiswa {
+
+private:
+    int nim;
+    string nama;
+public:
+    void inputdata() {
+        cout << "Masukkan NIM : ";
+        cin >> nim;
+        cout << "Masukkan Nama : ";
+        cin >> nama;
+    }
+    void tampildata() {
+        cout << "\nNIM : " << nim << endl;
+        cout << "Nama : " << nama << endl;
+    }
+};
+
+class Matakuliah {
+private:
+    string kode;
+    string namamk;
+    int sks;
+
+public:
+    void InputMK();
+    void TampilMK();
+
+};
+
+void Matakuliah::InputMK() {
+    cout << "\nMasukkan Kode Mata Kuliah : ";
+    cin >> kode;
+    cout << "Masukkan Nama Mata Kuliah : ";
+    cin >> namamk;
+    cout << "Masukkan Sks Mata Kuliah : ";
+    cin >> sks;
+}
+
+void Matakuliah::TampilMK() {
+    cout << "\nKode : " << kode << endl;
+    cout << "Nama Matakuliah : " << namamk << endl;
+    cout << "Jumlah SKS : " << sks << endl;
+}
+
+int main()
+{
+    Mahasiswa mhs;
+    Matakuliah mk;
+
+    mhs.inputdata();
+    mhs.tampildata();
+
+    mk.InputMK();
+    mk.TampilMK();
+
+}
